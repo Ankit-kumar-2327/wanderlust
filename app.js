03d604d80593009e08d6217eba6334b48f6bef66
Ankit Kumar <ankitkumar802216k@gamil.com>
2026-08-11 15:50:23 +0530

    Rout For JWT based Authentication

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-if(process.env.NODE_ENV != "production") {
-    require("dotenv").config(); 
-} 
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
 
-const express = require("express");  //ejs bydefault required by express
+const express = require("express"); //ejs bydefault required by express
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
@@ -15,71 +15,78 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
 const session = require("express-session");
-const MongoStore = require('connect-mongo').default;
+const MongoStore = require("connect-mongo").default;
+const cookieParser = require("cookie-parser");
 console.log(MongoStore);
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/users.js");
 
-app.engine("ejs" , ejsMate); 
+app.engine("ejs", ejsMate);
 app.use(methodOverride("_method"));
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 const DB_URL = process.env.ATLAS_MONGODB_URL;
 
-main().then(() => {console.log("Database connected sucessfully");})
-.catch((err) => { console.log(err);});
+main()
+  .then(() => {
+    console.log("Database connected sucessfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-async function main(){  // returns promise so then and catch method apply
-    await mongoose.connect(DB_URL);
+async function main() {
+  // returns promise so then and catch method apply
+  await mongoose.connect(DB_URL);
 }
 
 const store = MongoStore.create({
-    mongoUrl: DB_URL,
-    crypto: {
-        secret: process.env.SECRET,
-    },
-    touchAfter: 24 * 60 * 60  
+  mongoUrl: DB_URL,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 60 * 60,
 });
 store.on("error", () => {
-    console.log("Error in MongoSession Store", error);
-})
+  console.log("Error in MongoSession Store", error);
+});
 const sessionOptions = {
-    store,
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        expire: Date.now() * 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-    }
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expire: Date.now() * 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 };
-app.get("/", (req,res) => {
-    res.redirect("/listings");
+app.get("/", (req, res) => {
+  res.redirect("/listings");
 });
 
 app.use(session(sessionOptions));
 app.use(flash());
 
- // USER AUTHENTICATION
+// USER AUTHENTICATION
 app.use(passport.initialize());
 app.use(passport.session()); // same user in different tab but same session
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-app.use((req,res,next) => { 
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
-    next();
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
 });
-
 
 // RESTRUCTURING THE LISTINGS AND REVIEW
 app.use("/listings", listingRouter);
@@ -87,11 +94,10 @@ app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
 app.use((err, req, res, next) => {
-   let { statusCode = 500, message = "Something went wrong" } = err;
-   res.render("error.ejs", { err });
+  let { statusCode = 500, message = "Something went wrong" } = err;
+  res.render("error.ejs", { err });
 });
 
 app.listen(8080, () => {
-    console.log("server is listening to port 8080 ");
+  console.log("server is listening to port 8080 ");
 });
-

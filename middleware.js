582e5e420f98d36ac933331d0091c348e2d0459b
Ankit Kumar <ankitkumar802216k@gamil.com>
2026-08-11 15:51:57 +0530

    Adding jsonwebToken package

--- middleware.js
+++ middleware.js
@@ -1,60 +1,99 @@
 const Listing = require("./models/listing.js");
 const ExpressError = require("./utils/ExpressError.js");
 const Review = require("./models/review.js");
-const {listingSchema, reviewSchema} = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const { verifyToken } = require("./utils/jwt.js");
 
 module.exports.isLoggedIn = (req, res, next) => {
-    if(!req.isAuthenticated()){
-        req.session.redirectUrl = req.originalUrl;
-        req.flash("error" , "you must be logged in to create a new listings!");
-        return res.redirect("/login");
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  let token;
+  const authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    token = authHeader.slice(7);
+  }
+  if (!token && req.cookies) {
+    token = req.cookies.jwt;
+  }
+  if (token) {
+    try {
+      const payload = verifyToken(token);
+      req.user = { _id: payload.id, username: payload.username };
+      return next();
+    } catch (err) {
+      // fall through to redirect if token is invalid
     }
-    next();
+  }
+  req.session.redirectUrl = req.originalUrl;
+  req.flash("error", "you must be logged in to create a new listings!");
+  return res.redirect("/login");
+};
+
+module.exports.authenticateToken = (req, res, next) => {
+  let token;
+  const authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    token = authHeader.slice(7);
+  }
+  if (!token && req.cookies) {
+    token = req.cookies.jwt;
+  }
+  if (!token) {
+    return res.status(401).json({ error: "Authentication token missing" });
+  }
+  try {
+    const payload = verifyToken(token);
+    req.user = { _id: payload.id, username: payload.username };
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid or expired token" });
+  }
 };
 
 module.exports.saveRedirectUrl = (req, res, next) => {
-    if(req.session.redirectUrl) {
-        res.locals.redirectUrl = req.session.redirectUrl; //
-    }
-    return next();
+  if (req.session.redirectUrl) {
+    res.locals.redirectUrl = req.session.redirectUrl; //
+  }
+  return next();
 };
 
 module.exports.isOwner = async (req, res, next) => {
-    let { id } = req.params;
-    let listing = await Listing.findById(id);
-    if(!listing.owner._id.equals(res.locals.currUser._id)) {
-        req.flash("error", "you are not the owner of this listing");
-        return res.redirect(`/listings/${id}`);
-    }
-    next(); // to transfer to other error handling middleware.
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  if (!listing.owner._id.equals(res.locals.currUser._id)) {
+    req.flash("error", "you are not the owner of this listing");
+    return res.redirect(`/listings/${id}`);
+  }
+  next(); // to transfer to other error handling middleware.
 };
 
-module.exports.validateListing =  (req, res, next) => {
-    let {error} = listingSchema.validate(req.body);
-    if(error){
-        let msg = error.details.map((el) => el.message).join(","); 
-        throw new ExpressError(400, msg);
-    } else {
-        return next();
-    }
+module.exports.validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, msg);
+  } else {
+    return next();
+  }
 };
 
-module.exports.validateReview = (req,res,next) => {
-    let {error} = reviewSchema.validate(req.body);
-    if(error){
-        let msg = error.details.map((el) => el.message).join(",");  // if there are multiple errors then we will join them with comma
-        throw new ExpressError(400, msg);
-    }else{
-        return next();
-    }
+module.exports.validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  if (error) {
+    let msg = error.details.map((el) => el.message).join(","); // if there are multiple errors then we will join them with comma
+    throw new ExpressError(400, msg);
+  } else {
+    return next();
+  }
 };
 
 module.exports.isReviewAuthor = async (req, res, next) => {
-    let { id, reviewId} = req.params;
-    let review = await Review.findById(reviewId);
-    if(!review.createdBy._id.equals(res.locals.currUser._id)) {
-        req.flash("error" , "you are not the author of this review");
-        return res.redirect(`/listings/${id}`);
-    }
-    next();
-}
+  let { id, reviewId } = req.params;
+  let review = await Review.findById(reviewId);
+  if (!review.createdBy._id.equals(res.locals.currUser._id)) {
+    req.flash("error", "you are not the author of this review");
+    return res.redirect(`/listings/${id}`);
+  }
+  next();
+};

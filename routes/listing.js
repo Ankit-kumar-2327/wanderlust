a6e49215437bd4eae39949c89516ca8438618cd5
Ankit Kumar <ankitkumar802216k@gamil.com>
2026-08-20 15:16:00 +0530

    Created reserve route

--- routes/listing.js
+++ routes/listing.js
@@ -1,43 +1,45 @@
-const express = require("express");  // .. means go into parent directory
+const express = require("express"); // .. means go into parent directory
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
-const { isLoggedIn, isOwner, validateListing }  = require("../middleware.js");
-const listingController  = require("../controllers/listings.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const listingController = require("../controllers/listings.js");
 const multer = require("multer");
 const { storage } = require("../cloudconfig.js");
 const upload = multer({ storage });
 
-
 //INDEX ROUTE
-router.route("/")
-.get(wrapAsync(listingController.index))
-.post(isLoggedIn,
-    //validateListing,
-    upload.single("listing[image]"),
-    wrapAsync(listingController.createListing)
+router.route("/").get(wrapAsync(listingController.index)).post(
+  isLoggedIn,
+  //validateListing,
+  upload.single("listing[image]"),
+  wrapAsync(listingController.createListing),
 );
 
 //NEW ROUTE
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 //"/:id" path
-router.route("/:id")
-.get(wrapAsync(listingController.showListing))
-.put( 
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing))
+  .put(
     isLoggedIn,
     isOwner,
     upload.single("listing[image]"),
     validateListing,
-    wrapAsync(listingController.updateListing)
-)
-.delete( 
-    isLoggedIn, 
-    isOwner,
-    wrapAsync(listingController.destroyListing)
-);
+    wrapAsync(listingController.updateListing),
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
+
+router.get("/:id/reserve", wrapAsync(listingController.renderReservePage));
 
 //EDIT and UPDATE ROUTE
-router.get("/:id/edit", isLoggedIn, isOwner,wrapAsync(listingController.editListingForm));
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingController.editListingForm),
+);
 
 module.exports = router;
